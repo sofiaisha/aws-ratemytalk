7b5ae5fcc465c771b315f4e8c922c9ae90685bcb
Boaz Ziniman <boazz@amazon.com>
2017-12-25 00:18:21 +0200

    Adding final confermation

--- getTalks.py
+++ getTalks.py
@@ -138,7 +138,7 @@ def getMyTalks(event, context):
 
     if session_date and session_name and session_score:
         logger.info(event['currentIntent']['slots'])
-        confirm_intent(None, intent, event['currentIntent']['slots'],
+        return confirm_intent(None, intent, event['currentIntent']['slots'],
         {'contentType': 'PlainText', 'content': 'Are you sure with sending the score %s for the session %s on %s?' % (session_score, session_name, session_date)}, None)
 
     else:
