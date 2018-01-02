da613d73bcf38f6564d0a6b8435ddc0e328a54c8
Boaz Ziniman <boazz@amazon.com>
2018-01-03 01:29:24 +0200

    Fix reference to last_month

--- getTalks.py
+++ getTalks.py
@@ -174,7 +174,7 @@ def get_my_talks(event, context):
 
         else:
             last_month = datetime.now() - timedelta(days=30)
-            last_month = lookup_val.strftime("%Y-%m-%d")
+            last_month = last_month.strftime("%Y-%m-%d")
 
             mySession = get_session(last_month)
             return elicit_slot(None, intent, event['currentIntent']['slots'], 'sessionName',
